9c20c3927e313e4cbdb675bced1e89db1e71f5ba
ivsnp <65447880+ivsnp@users.noreply.github.com>
2022-12-04 11:38:37 +0100

    portafoglio init

---
+++ portafoglio/src/main/java/com/homates/portafoglio/PortafoglioApplication.java
@@ -0,0 +1,13 @@
+package com.homates.portafoglio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafoglioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafoglioApplication.class, args);
+	}
+
+}
