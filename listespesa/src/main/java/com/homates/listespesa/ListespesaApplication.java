67145f69e4359d7768cc22c1be472be59737e7d3
ivsnp <65447880+ivsnp@users.noreply.github.com>
2022-12-04 11:37:36 +0100

    listespesa

---
+++ listespesa/src/main/java/com/homates/listespesa/ListespesaApplication.java
@@ -0,0 +1,13 @@
+package com.homates.listespesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListespesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListespesaApplication.class, args);
+	}
+
+}
