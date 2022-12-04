37a486b2c37548d853dd22b62b2015c790c51139
ivsnp <65447880+ivsnp@users.noreply.github.com>
2022-12-04 11:42:08 +0100

    bacheca init

---
+++ bacheca/src/main/java/com/homates/bacheca/BachecaApplication.java
@@ -0,0 +1,13 @@
+package com.homates.bacheca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachecaApplication.class, args);
+	}
+
+}
---
+++ bacheca/src/test/java/com/homates/bacheca/BachecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homates.bacheca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
