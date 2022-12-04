aa01ff1975f13e1ab2455b1b61036376a56e93b7
ivsnp <65447880+ivsnp@users.noreply.github.com>
2022-12-04 11:40:30 +0100

    calendario init

---
+++ calendario/src/main/java/com/homates/calendario/CalendarioApplication.java
@@ -0,0 +1,13 @@
+package com.homates.calendario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarioApplication.class, args);
+	}
+
+}
---
+++ calendario/src/test/java/com/homates/calendario/CalendarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homates.calendario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
