81fe2b1dba41910ae00eac9c1455ee78904c6b0f
ivsnp <65447880+ivsnp@users.noreply.github.com>
2022-12-08 11:01:42 +0100

    ShoppingList CRUD

--- listespesa/src/main/java/com/homates/listespesa/controller/ShoppingListController.java
+++ listespesa/src/main/java/com/homates/listespesa/controller/ShoppingListController.java
@@ -3,10 +3,13 @@ package com.homates.listespesa.controller;
 import com.homates.listespesa.model.ShoppingList;
 import com.homates.listespesa.repo.ShoppingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -31,4 +34,46 @@ public class ShoppingListController {
         ShoppingList _shoppingList = repository.save(new ShoppingList(shoppingList.getName(), shoppingList.getProductList()));
         return _shoppingList;
     }
+
+    @DeleteMapping("/shopping-lists/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable("id") long id) {
+        System.out.println("Shopping list with ID = " + id + "...");
+
+        repository.deleteById(id);
+
+        return new ResponseEntity<>("Shopping list has been deleted!", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/shopping-lists/delete")
+    public ResponseEntity<String> deleteAllCustomers() {
+        System.out.println("Delete shopping lists...");
+
+        repository.deleteAll();
+
+        return new ResponseEntity<>("All shopping lists have been deleted!", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/shopping-lists/name/{name}")
+    public List<ShoppingList> findByName(@PathVariable String name) {
+
+        List<ShoppingList> _shoppingList = repository.findByName(name);
+        return _shoppingList;
+    }
+
+    @PutMapping("/shopping-lists/update-name/{id}/{name}")
+    public ResponseEntity<ShoppingList> updateCustomer(@PathVariable("id") long id, @PathVariable("name") String name) {
+        System.out.println("Update shopping list name with ID = " + id + "...");
+
+        Optional<ShoppingList> shoppingList = repository.findById(id);
+
+        if (shoppingList.isPresent()) {
+            ShoppingList _shopL = shoppingList.get();
+            _shopL.setName(name);
+            return new ResponseEntity<>(repository.save(_shopL), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // TODO: add and remove items from list by ID
 }
