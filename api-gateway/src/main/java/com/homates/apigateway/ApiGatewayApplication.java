9aab30e4b51cada89850391456ec61c82f561837
ivsnp <ivanspada.mail@gmail.com>
2023-03-30 22:36:29 +0200

    gateway annotations and dependencies

--- api-gateway/src/main/java/com/homates/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/homates/apigateway/ApiGatewayApplication.java
@@ -2,14 +2,13 @@ package com.homates.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
