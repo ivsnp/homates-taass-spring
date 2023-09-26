01278c42882a28e1ceec81b13aae75b7aadeadf5
ivsnp <ivanspada.mail@gmail.com>
2023-09-26 15:44:28 +0200

    react test api requests

--- user-houses/src/main/java/com/homates/userhouses/controller/HouseController.java
+++ user-houses/src/main/java/com/homates/userhouses/controller/HouseController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://react-desktop:3000")
 @RestController
 @RequestMapping("/api/v1/user-houses")
 public class HouseController {
