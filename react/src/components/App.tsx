7193e332665e538593306a028c414aba5ae31fe9
matti <mattialadisa37@gmail.com>
2024-02-19 15:57:59 +0100

    Working on the login

--- react/src/components/App.tsx
+++ react/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import AppUser from "./user-logged/AppUser";
 import AppGuest from "./guest/AppGuest";
@@ -7,10 +7,20 @@ import Header from "./user-logged/header/Header";
 import HeaderGuest from "./guest/headerGuest/HeaderGuest";
 import Footer from "./footer/Footer";
 import Login from "./guest/login/Login";
+import { gapi } from 'gapi-script';
 
+const clientId = "903884998155-d5fqjb5mj7n5202e7qbdj3r9d3citfgj.apps.googleusercontent.com"
 function App() {
     const logged: boolean = true;
-
+    useEffect(() => {
+        function start(){
+            gapi.client.init({
+                clientId: clientId,
+                scope: ""
+            })
+        }
+        gapi.load('client:auth2', start)
+    });
     return (
         <Router>
             <div className="App">
